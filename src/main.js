840dae7e3610863c54a373346684e9433524054a
unknown <t4@gmail.com>
2018-09-11 13:56:47 +0800

    SPA with 2 routes

--- src/main.js
+++ src/main.js
@@ -1,18 +1,39 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import vueResource from 'vue-resource'
-import router from './router'
+// import router from './router'
 import App from './App'
+import Users from './components/Users'
+import Test from './components/Test'
 
 Vue.use(vueResource)
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/', component: Users },
+  { path: '/test', component: Test }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: __dirname,
+  routes: routes
+})
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
-})
+  template: `
+    <div id="app">
+      <ul>
+        <li><router-link to="/">Users</router-link></li>
+        <li><router-link to="/test">Test</router-link></li>
+      </ul>
+      <router-view></router-view>
+    </div>
+  `
+}).$mount('#app')
